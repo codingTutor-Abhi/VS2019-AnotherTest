e7621ef3e86a1ee3fdbac4793c9ec7648b7e87d3
codingTutor-Abhi <abhijeet.dholey@gmail.com>
2021-08-25 16:46:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS2019_AnotherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World again LOL !");
+        }
+    }
+}
